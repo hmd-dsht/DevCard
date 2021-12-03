45dda0f61c602b3508dbbbd714bb9b64e24d5ee0
hmd-dsht <hmd.dsht@gmail.com>
2021-12-03 23:56:28 +0330

    Index & Contact page added

--- DevCard_Mvc/Controllers/HomeController.cs
+++ DevCard_Mvc/Controllers/HomeController.cs
@@ -1,21 +1,16 @@
 ﻿using DevCard_Mvc.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc; 
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+      
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+           
         }
 
         public IActionResult Index()
@@ -23,10 +18,12 @@ namespace DevCard_Mvc.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
