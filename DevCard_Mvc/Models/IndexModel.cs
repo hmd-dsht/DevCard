de7ee08f178dd92a73c40ceb16178edaaead6c02
hmd-dsht <hmd.dsht@gmail.com>
2021-12-05 21:20:17 +0330

    partial View

---
+++ DevCard_Mvc/Models/IndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace DevCard_Mvc.Models
+{
+    public class IndexModel
+    {
+        public List<Project>Projects { set; get; }
+        public List<Article> Articles { get; set; }
+        
+    }
+}
---
+++ DevCard_Mvc/Models/Projects.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_Mvc.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Client { get; set; }
+    }
+
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+
+   
+}
